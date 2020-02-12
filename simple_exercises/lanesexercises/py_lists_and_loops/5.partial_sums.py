6c7b048608ee08dc1b77774388e5e6d362ddfb91
Immanuela Engländer <ila@Immanuelas-Air.station>
2020-02-12 22:06:21 +0100

    uploaded rest of the exercizes

--- simple_exercises/lanesexercises/py_lists_and_loops/5.partial_sums.py
+++ simple_exercises/lanesexercises/py_lists_and_loops/5.partial_sums.py
@@ -15,20 +15,3 @@ def partsumlist(li):
 
 y=partsumlist(li)
 print(y)
-
-# # 6. write a function that takes an integer n and prints a square of n*n #
-# # 7. print the multiplication table
-
-#5,write a function that takes a list of integers and computes the list of its partial sums
-# sahars answer
-# def partialsum():
-#     p = input("Enter some numbers please: ")
-#     pl=p.split()
-#     print (pl) 
-#     sum=0
-#     result=[]
-#     for x in pl:
-#         sum += int(x)
-#         result.append(sum)
-#     print ("the partial sum is",result)
-# partialsum()
---
+++ simple_exercises/lanesexercises/py_lists_and_loops/6.squared_n.py
@@ -0,0 +1,7 @@
+# # 6. write a function that takes an integer n and prints a square of n*n #
+def quadrat(n):
+    sq=n*n
+    print(sq)
+    return sq
+
+quadrat(10)
