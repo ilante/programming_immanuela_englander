fac0a80bfb8709109e31811ffb8ceed7cd03c30c
Immanuela Engländer <ila@Immanuelas-Air.station>
2020-02-13 13:53:28 +0100

    updates

--- simple_exercises/lanesexercises/py_if_and_files/16-18_input_string_complement.py
+++ simple_exercises/lanesexercises/py_if_and_files/16-18_input_string_complement.py
@@ -6,8 +6,9 @@ string1=string1.upper()
 string2=input('please input a second string')
 string2=string2.upper()
 # print(string2)
-dict-seq={
+dict-seq={"A":"T", "T":"A", "G":"C", "C":"G"}
 # 17. check if one string is the complement of the other (i.e. “AC” and “TG” -> yes)
+
 # 18. print 3 rows: the first and the third for the sequences, the second one should contain “|” if
 # the bases are complementary, “X” if not. Example:
 # ATTCGT
