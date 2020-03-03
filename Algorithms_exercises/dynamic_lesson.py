96b9a4d6fff66bd922e26ed42cd64f826dd5ce09
Immanuela Engländer <ila@Immanuelas-MacBook-Air.local>
2020-03-03 11:21:41 +0100

    ryddeaksjona sletta dobbelte

--- Algorithms_exercises/dynamic_lesson.py
+++
@@ -1,7 +0,0 @@
-# def score.m(s1, s2, S, d):
-
-# init F to the 0 matrix
-# init F (i,0)
-# init F(0,j)
-# iteration
-# return F
--- simple_exercises/py_lists_and_loops/1.py
+++
@@ -1,5 +0,0 @@
-# 1. merge the lists 
-l1=[4,8,-9,"the"]  
-l2 = ["silent force", 4.67, 9]
-l3=l1+l2 # lists can be joined by addition 
-print(l3)
